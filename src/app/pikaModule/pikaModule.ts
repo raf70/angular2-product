56605af57f62069838b5d377659e5caab04759e5
Rafal Kulczycki <rkulczycki@kanatek.com>
2017-01-04 14:13:17 -0500

    some basic coding

--- src/app/pikaModule/pikaModule.ts
+++ src/app/pikaModule/pikaModule.ts
@@ -1,10 +1,18 @@
 export class PikaModule {
     constructor(
         public id: number,
-        public parentId: number,
-        public item_no: string,
-        public desc: string,
         public sn: string,
-        public parentSn: string
+        public itemNo: string,
+        public desc1: string,
+        public mac0: string,
+        public mac1: string,
+        public uuid0: string,
+        public uuid1: string,
+        public imei0: string,
+        public imei1: string,
+        public parentSn: string,
+        public parentId: number,
+        public isParent: boolean,
+        public createDt: Date
     ){}
 }
